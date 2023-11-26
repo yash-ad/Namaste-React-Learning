7577add0787d6a0a63834871b00f6b2f320fc060
Yash Dandnaik <yashrajdandnaik.com>
2023-11-26 20:44:13 +0530

    practicing on project

--- Chapter 04-Talk is Cheap Show me the code/Code/src/components/Body.js
+++ Chapter 04-Talk is Cheap Show me the code/Code/src/components/Body.js
@@ -5,8 +5,10 @@ const Body = ()=>{
     return(
 <div className='body'>
 <div className='search-container'>
+    <div className="search-bar">
       <input type="text" placeholder="Satisfy your hunger now!" id="searchInp"></input>
       <button id="searchBtn">Search</button>
+      </div>
     </div>
 <div className='restaurant-container'> 
 {restaurantList.map((restaurant)=>(
