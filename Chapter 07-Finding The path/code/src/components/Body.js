02df6328446a4aa19c31c8d732fe07bb24dffe68
Yash Dandnaik <yashrajdandnaik.com>
2023-12-04 18:47:09 +0530

    Cleaning and maintainig the code using chatgpt,it really helps well with the comments

--- Chapter 07-Finding The path/code/src/components/Body.js
+++ Chapter 07-Finding The path/code/src/components/Body.js
@@ -1,87 +1,111 @@
+// Importing necessary dependencies and components
 import RestaurantCard from "./RestaurantCard";
-import { useState , useEffect } from "react";
+import { useState, useEffect } from "react";
 import Shimmer from "./Shimmer";
 import { CORS_URL } from "../utilities/config";
-import { API_URL} from "../utilities/config";
+import { API_URL } from "../utilities/config";
 import { Link } from "react-router-dom";
 
-const Body = ()=>{
+// Defining the Body component
+const Body = () => {
+  // State variables using the useState hook
+  const [listOfRestaurants, setlistOfRestaurants] = useState([]);
+  const [filterRestaurants, setFilterRestaurants] = useState([]);
+  const [searchText, setSearchText] = useState('');
 
-const [listOfRestaurants,setlistOfRestaurants] = useState([]);
-const [filterRestaurants,setFilterRestaurants] = useState([]);
-const [searchText,setSearchText] = useState('');
+  // useEffect hook to fetch data when the component mounts
+  useEffect(() => {
+    fetchData();
+  }, []);
 
+  // Async function to fetch restaurant data
+  const fetchData = async () => {
+    try {
+      // Fetching data from the specified API
+      const data = await fetch(CORS_URL + API_URL);
+      const json = await data.json();
 
-useEffect(()=>{
-fetchData()
-},[]);
-
-const fetchData = async () => {
-try{
-    const data = await fetch( CORS_URL + API_URL); 
-
-    const json = await data.json();
-    
-    setlistOfRestaurants(json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
-    
-    setFilterRestaurants(json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
+      // Setting the state variables with restaurant data
+      setlistOfRestaurants(json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+      setFilterRestaurants(json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+    } catch (error) {
+      console.error('Error Fetching Data:', error);
     }
-    catch(error){
-        console.error('Error Fetching Data:',error)
-    }
+  };
+
+  // Display a loading shimmer if there are no filtered restaurants yet
+  if (filterRestaurants.length === 0) return <Shimmer />;
+
+  return (
+    <div className="body">
+      {/* Search Bar */}
+      <div className="search-container">
+        <div className="search-bar">
+          <input
+            type="text"
+            placeholder="Satisfy your hunger now!"
+            id="searchInp"
+            value={searchText}
+            onChange={(event) => {
+              setSearchText(event.target.value);
+            }}
+          />
+          <button
+            id="searchBtn"
+            onClick={() => {
+              // Filter restaurants based on search text
+              let filterSearch = listOfRestaurants.filter((res) =>
+                res.info.name.toLowerCase().includes(searchText.toLowerCase())
+              );
+              if (filterSearch.length === 0) {
+                // Handle case when no restaurants match the search
+              } else {
+                setFilterRestaurants(filterSearch);
+              }
+            }}
+          >
+            Search
+          </button>
+        </div>
+      </div>
+
+      {/* Filter Buttons */}
+      <div className="filter-Buttons">
+        <button
+          className="finder-btns"
+          onClick={() => {
+            // Filter restaurants based on ratings greater than 4.0
+            const filteredLists = listOfRestaurants.filter((res) => res.info.avgRating > 4);
+            setFilterRestaurants(filteredLists);
+          }}
+        >
+          Ratings 4.0+
+        </button>
+
+        <button
+          className="finder-btns"
+          onClick={() => {
+            // Filter restaurants to show only pure veg
+            const findVegRestaurants = listOfRestaurants.filter((res) => res.info.veg === true);
+            setFilterRestaurants(findVegRestaurants);
+          }}
+        >
+          Pure Veg
+        </button>
+      </div>
+
+      {/* Displaying Restaurant Cards */}
+      <div className="restaurant-container">
+        {filterRestaurants?.map((restaurant) => (
+          // Linking each restaurant card to its details page
+          <Link key={restaurant.info.id} to={'/restaurants/' + restaurant.info.id}>
+            <RestaurantCard restaurantData={restaurant} />
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
 };
 
-
-if (filterRestaurants.length === 0) return <Shimmer />;
-     
-
-return(
-<div className="body">
-
-
-<div className="search-container">
-<div className="search-bar">
-<input type="text" placeholder="Satisfy your hunger now!" id="searchInp" value={searchText} onChange={(event)=>{
-setSearchText(event.target.value);
-}}>
-</input>
-<button id="searchBtn" onClick={()=>{
-let filterSearch = listOfRestaurants.filter((res)=>
-res.info.name.toLowerCase().includes(searchText.toLowerCase())
-)
-if(filterSearch.length === 0){
-}
-else{
-setFilterRestaurants(filterSearch)
-}}}>Search
-</button>
-</div>
-</div>
-
-
-
-<div className="filter-Buttons">
-<button className="finder-btns" 
-onClick={() => {const filteredLists = listOfRestaurants.filter((res)=> res.info.avgRating > 4)
-setFilterRestaurants(filteredLists)
-}}>Ratings 4.0+
-</button>
-
-<button className="finder-btns" onClick={()=>{const findVegRestaurants = listOfRestaurants.filter((res)=>(
-res.info.veg === true))
-setFilterRestaurants(findVegRestaurants)
-        }}>Pure Veg
-</button>     
-</div>
-
-<div className="restaurant-container">
-        {filterRestaurants?.map((restaurant)=>(
-          <Link key={restaurant.info.id} to={'/restaurants/'+ restaurant.info.id}><RestaurantCard restaurantData={restaurant}/></Link> 
-        ))} 
-</div>
-
-</div>
-)
-};
-
+// Exporting the Body component
 export default Body;
--- Chapter 07-Finding The path/code/src/components/Header.js
+++ Chapter 07-Finding The path/code/src/components/Header.js
@@ -1,29 +1,45 @@
+// Importing necessary dependencies and configuration
 import { LOGO_URL } from "../utilities/config";
 import { useState } from "react";
 import { Link } from "react-router-dom";
 
-const Header = ()=>{
-    const [toggleButton,setToggleButton] = useState('Login');
+// Defining the Header component
+const Header = () => {
+  // Using the useState hook to manage the state of the toggle button
+  const [toggleButton, setToggleButton] = useState('Login');
 
-    return(
+  return (
+    // JSX representing the structure of the Header component
     <div className="header">
-    <div className="logo-container">
-  <Link to="/"><img className="logo" alt="foodapplogo" src={LOGO_URL}/></Link>
+      <div className="logo-container">
+        {/* Linking the logo to the home page */}
+        <Link to="/">
+          <img className="logo" alt="foodapplogo" src={LOGO_URL} />
+        </Link>
+      </div>
+      <div className="nav-items">
+        <ul>
+          {/* Navigation links using the Link component from React Router */}
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About Us</Link></li>
+          <li><Link to="/contact">Contact Us</Link></li>
+          <li>Cart</li>
+          {/* Toggle button for login/logout functionality */}
+          <button
+            className="toggleBtn"
+            onClick={() => {
+              // Toggling the button text between 'Login' and 'Logout'
+              toggleButton === 'Login' ?
+                setToggleButton('Logout') : setToggleButton('Login')
+            }}
+          >
+            {toggleButton}
+          </button>
+        </ul>
+      </div>
     </div>
-    <div className="nav-items">
-    <ul>
-    <li><Link to="/">Home</Link></li>
-    <li><Link to="/about">About Us</Link></li>
-    <li><Link to="/contact">Contact Us</Link></li>
-    <li>Cart</li>
-    <button className="toggleBtn" onClick={()=>{
-toggleButton === 'Login'?
-setToggleButton('Logout'):setToggleButton('Login')
-    }}>{toggleButton}</button>
-    </ul>
-    </div>
-    </div>
-    )
-    };
-    
+  );
+};
+
+// Exporting the Header component to make it available for other parts of the application
 export default Header;
