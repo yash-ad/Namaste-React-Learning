eb30db7fbe1b3073f728f5c0dcb8883f11bbb7d6
Yash Dandnaik <yashrajdandnaik.com>
2023-12-06 14:48:32 +0530

    Understanding of ` this.setState()`

--- Chapter 08-Lets get classy/code/src/components/About.js
+++ Chapter 08-Lets get classy/code/src/components/About.js
@@ -1,12 +1,11 @@
 import User from "./User";
 import UserClass from "./UserClass";
 
-const About = ()=>{
-    
+const About = ()=>{ 
 return(
 <div className="about-us-container">
 <h1>About Us</h1> 
-<User name={"Yash dandnaik (functional)"} location={"Pune (functional)"}/>
+{/* <User name={"Yash dandnaik (functional)"} location={"Pune (functional)"}/> */}
 <UserClass name={"Yash dandnaik (class)"} location={"Pune (class)"}/>
 </div>
 )
--- Chapter 08-Lets get classy/code/src/components/UserClass.js
+++ Chapter 08-Lets get classy/code/src/components/UserClass.js
@@ -2,16 +2,25 @@ import React from "react";
 
 class UserClass extends React.Component{
 
-    constructor(props){
+ constructor(props){   
 super(props)
+this.state = {
+count : 1,
+message:"Hello React,Im from Classbased component"
+};
     };
 
 render(){
     const{name,location} =  this.props;
+    const{count,message} = this.state;
     return(
-        <div className="user-container">
+        <div className="user-card">
         <div>Name:{name}</div>
         <div>Location:{location}</div>
+        {/* <h3>Count:{this.state.count}</h3>
+        <h4>Message:{this.state.message}</h4> */}
+        <h3>Count:{count}</h3>
+        <h4>Message:{message}</h4>
         </div>
         )
 }
