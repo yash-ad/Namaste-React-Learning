7cb7df126dff12c050ca51e2dbda6a58069c6c09
Yash Dandnaik <yashrajdandnaik.com>
2023-12-06 15:12:43 +0530

    playing with the browser console and class based component and updates the variable using `this.setState({})`

--- Chapter 08-Lets get classy/code/src/components/UserClass.js
+++ Chapter 08-Lets get classy/code/src/components/UserClass.js
@@ -5,8 +5,9 @@ class UserClass extends React.Component{
  constructor(props){   
 super(props)
 this.state = {
-count : 1,
-message:"Hello React,Im from Classbased component"
+count : 0,
+message:"Hello React,Im in a `this.state{}` In a constructor special method from Classbased component"
+
 };
     };
 
@@ -19,7 +20,15 @@ render(){
         <div>Location:{location}</div>
         {/* <h3>Count:{this.state.count}</h3>
         <h4>Message:{this.state.message}</h4> */}
+
+        {/* Whenever a state variable updates,react triggers a reconciliation process or cycle it will re-renders the component. */}
         <h3>Count:{count}</h3>
+        <button onClick={()=>{
+this.setState({
+count:this.state.count + 1,
+message:"Hello React ,Tadaaa , Im using `this.setState()` to update myself",
+})
+        }}>Click Here</button>
         <h4>Message:{message}</h4>
         </div>
         )
