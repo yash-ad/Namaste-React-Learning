68977c3419401dd40321dc54cb14101c9c6ebaec
Yash Dandnaik <yashrajdandnaik.com>
2023-12-24 21:11:29 +0530

    code updated

--- Chapter 12-Lets build our store/code/src/components/ItemsList.js
+++ Chapter 12-Lets build our store/code/src/components/ItemsList.js
@@ -11,12 +11,12 @@ const ItemsList = (props)=>{
 
 
 //This is a designated callback function.handleAddItem();
-const handleAddItem = ()=>{
+const handleAddItem = (item)=>{
   //In the action whatever i pass into this action it will be display.
   //dispatch(addItem("pizza"))//['pizza']
   
 //`It dispatches an action`.
-dispatch(addItem())
+dispatch(addItem(item))
 };
 
     return(
@@ -32,7 +32,24 @@ dispatch(addItem())
                 <div className="menu-right">
                   <img src={IMG_URL + item.card.info.imageId} alt="Item" />
                   {/* //The `onClick` event will call a designated callback function when the button is clicked. */}
-                  <button onClick={handleAddItem} id="addBtn">ADD </button>
+                  
+                  
+                  {/* Without Arrow Function:In this case, the handleAddItem function will be called when the button is clicked.  */}
+                  {/* <button onClick={handleAddItem}  */}
+                  
+                  {/* //With Arrow Function (No Argument):Here, An arrow function is used to create an inline function that, when invoked, will call handleAddItem with the item argument. This is useful when you want to pass specific arguments to your event handler. */}
+                  <button onClick={()=> handleAddItem(item)} 
+
+                  //In this case, the handleAddItem function is not being used as an event handler for the onClick event. 
+                  // Instead, it is being immediately invoked (called) when the component renders.
+                  // This happens because of the parentheses () at the end of handleAddItem(item). 
+                  // This means that handleAddItem will be called with the argument item as soon as the component renders, not when the button is clicked.
+                  // If you want to pass arguments to the handleAddItem function when the button is clicked, you should use an arrow function or a function reference without invoking it immediately. 
+                  // {/* <button onClick={handleAddItem(item)}  */}
+                  
+                  
+                  
+                  id="addBtn">ADD </button>
                 </div>
               </div>
             ))}
