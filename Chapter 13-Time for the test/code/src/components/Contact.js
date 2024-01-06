29d3dffbd2bb92f45809ad721fd9540e1ce6bb95
Yash Dandnaik <yashrajdandnaik.com>
2024-01-06 15:28:10 +0530

    created a contact page with css

--- Chapter 13-Time for the test/code/src/components/Contact.js
+++ Chapter 13-Time for the test/code/src/components/Contact.js
@@ -3,20 +3,21 @@ const Contact = ()=>{
 return(
 <div className="contact-page">
     <div id="contact">
-    <img src="https://c1.wallpaperflare.com/preview/546/136/32/menu-pizza-vegetables-eat.jpg" alt=""/>
+    <img src="https://c1.wallpaperflare.com/preview/877/7/816/writing-pad-vegetables-table-tray.jpg" alt=""/>
     <div id="overlay"></div>
     <div id="contact-left">
     <h1>Reach out to us</h1>
     <p>Require help or have inquiries? Reach out to our approachable team for prompt and supportive assistance.</p>
     </div>
     <div id="contact-right">
-    <label>Name</label>
-    <input type="text" placeholder="Eg:Aditya Verma"/>
-    <label>Email</label>
-    <input type="email" placeholder="xyz@gmail.com"/>
+    <label className="name-label">Name</label>
+    <input type="text" placeholder="John Doe"/>
+    <label className="email-label">Email</label>
+    <input type="email" placeholder="johndoe@gmail.com"/>
     <label>Tell Us More</label>
-    <textarea placeholder="Message" cols="30" rows="10"></textarea>
+    <textarea placeholder="Message" cols="20" rows="10"></textarea>
     <button>Send Message</button>
+
     </div>
     </div>
 </div>
